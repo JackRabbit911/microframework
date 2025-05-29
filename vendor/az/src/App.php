9196d43f94ba33d30aade29b92bc0b7913c2f5ea
alx <world.zay@gmail.com>
2025-05-29 17:05:57 +0300

    Create Sys\App class

---
+++ vendor/az/src/App.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys;
+
+class App
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function run()
+    {
+        echo 'Hello';
+    }
+}
