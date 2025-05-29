e2eecc890e3a8739bd46810bb0bdeae1377e29a7
alx <world.zay@gmail.com>
2025-05-29 17:47:30 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,15 @@
 declare(strict_types=1);
 
 use Sys\App;
+use Sys\Container\Container;
+
+define('APPPATH', '../application/');
 
 require_once '../vendor/autoload.php';
 
+require_once '../vendor/az/src/library.php';
+
+$container = new Container();
+$container->addDefinitions();
 $app = new App;
 $app->run();
