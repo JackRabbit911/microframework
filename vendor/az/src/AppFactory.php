7814ee8ee5bd8da883f536bcb0054248af32ebc2
alx <world.zay@gmail.com>
2025-05-30 13:52:28 +0300

    Update AppFactory class

--- vendor/az/src/AppFactory.php
+++ vendor/az/src/AppFactory.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Sys;
 
-use Sys\Container\Container;
+use DI\ContainerBuilder;
 
 class AppFactory
 {
     public static function create(): App
     {
-        $container = new Container();
-        $container->addDefinitions('../vendor/az/src/Container/config.php');
+        $builder = new ContainerBuilder();
+        $builder->addDefinitions('../vendor/az/src/container.php');
+        $container = $builder->build();
         return $container->get(App::class);
     }
 }
