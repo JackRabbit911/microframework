83f9e3df2255e5572e2a621e27de321ad92de270
alx <world.zay@gmail.com>
2025-06-01 17:57:10 +0300

    Create ControllerAttribute middleware, update App.php

--- vendor/az/src/App.php
+++ vendor/az/src/App.php
@@ -7,10 +7,10 @@ namespace Sys;
 use Sys\Pipeline\PipelineInterface;
 use Az\Route\Middleware\RouteDispatch;
 use Az\Route\Middleware\RouteMatch;
-use Az\Route\RouterInterface;
 use HttpSoft\Emitter\EmitterInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Sys\Middleware\ControllerAttribute;
 use Sys\Pipeline\PostProcess;
 
 class App
@@ -18,7 +18,6 @@ class App
     public function __construct(
         private ServerRequestInterface $request,
         private PipelineInterface $pipeline,
-        private RouterInterface $router,
         private RequestHandlerInterface $defaultHandler,
         private PostProcess $postProcess,
         private EmitterInterface $emitter
@@ -28,6 +27,7 @@ class App
     {
         require_once APPPATH . 'config/pipeline.php';
         $this->pipeline->pipe(RouteMatch::class);
+        $this->pipeline->pipe(ControllerAttribute::class);
         $this->pipeline->pipe(RouteDispatch::class);
 
         $response = $this->pipeline->process($this->request, $this->defaultHandler);
