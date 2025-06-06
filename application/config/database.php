4f41ba05b994129e3ea60dc8a102951be4a73526
alx <world.zay@gmail.com>
2025-06-06 14:06:39 +0300

    Create database config

---
+++ application/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mysql' => [
+        'host' => env('DB_HOST'),
+        'database' => env('DB_DATABASE'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'charset' => env('DB_CHARSET'),
+        'root_password' => env('DB_ROOT_PASSWORD'),
+    ],
+];
