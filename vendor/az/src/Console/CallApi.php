f9717ec97aa7d2aa85d9a870a5255c0f016497b2
alx <world.zay@gmail.com>
2025-06-06 13:15:46 +0300

    Create Console\CallApi and Console\Controller classes

---
+++ vendor/az/src/Console/CallApi.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys\Console;
+
+final class CallApi
+{
+    private string $classname;
+    private string $method;
+    private string $query;
+
+    public function __construct(string $classname, string $method, ?array $args = null)
+    {
+        $this->classname = $classname;
+        $this->method = $method;
+        $this->query = ($args) ? '?' . http_build_query($args) : '';
+    }
+
+    public function execute(array $data = [])
+    {
+        $path = ltrim(str_replace('\\', '/', $this->classname), '/') . '/' . $this->method . $this->query;
+        $client = new \GuzzleHttp\Client(['base_uri' => env('APP_URL') . '/api/console/']);
+        $response = $client->post($path, ['body' => json_encode($data, JSON_UNESCAPED_UNICODE)]);
+        
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
