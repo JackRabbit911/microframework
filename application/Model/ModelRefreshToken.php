faf766acf4ff53af366bad70cb64f67bfbe9c203
alx <world.zay@gmail.com>
2025-06-09 14:19:26 +0300

    Update ModelRefreshToken and O2AuthRepo

--- application/Model/ModelRefreshToken.php
+++ application/Model/ModelRefreshToken.php
@@ -4,23 +4,46 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use PDO;
 use Pecee\Pixie\QueryBuilder\IQueryBuilderHandler;
 use Sys\Model\MysqlModel;
 
 class ModelRefreshToken extends MysqlModel
 {
-    private string $table;
+    private string $table = 'refresh_tokens';
 
     public function __construct(protected IQueryBuilderHandler $qb)
     {
         parent::__construct($qb);
     }
 
-    public function create(array $data){}
+    public function create(array $data)
+    {
+        $this->qb->table($this->table)->insert($data);
+    }
 
-    public function read(int $id){}
+    public function read(string $token, string $user_agent, int $lifetime)
+    {
+        $expire = date('Y-m-d h:i:s', time() - $lifetime);
 
-    public function update(int $id, array $data){}
+        return $this->qb->table($this->table)
+            ->select('token', 'user_id', 'updated')
+            ->where('user_agent', '=', $user_agent)
+            ->where('updated', '>', $expire)
+            ->find($token, 'token');
+    }
 
-    public function delete(int $id){}
+    public function update(string $token, array $data): void
+    {
+        $this->qb->table($this->table)
+            ->where('token', '=', $token)
+            ->update($data);
+    }
+
+    public function delete(string $token): void
+    {
+        $this->qb->table($this->table)
+            ->where('token', '=', $token)
+            ->delete();
+    }
 }
--- application/Repository/O2AuthRepo.php
+++ application/Repository/O2AuthRepo.php
@@ -4,35 +4,52 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use stdClass;
+use App\DTO\User;
+use App\Model\ModelRefreshToken;
+use Firebase\JWT\ExpiredException;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use UnexpectedValueException;
 
 class O2AuthRepo
 {
     private array $config;
 
-    public function __construct()
+    public function __construct(private ModelRefreshToken $model)
     {
         $this->config = config('o2auth');
     }
 
-    public function generateAccessToken()
+    public function generateRefreshToken($user_id)
     {
-        $user = new stdClass;
-        $user->id = 1;
-        $user->name = 'Алексей';
+        $data['user_agent'] = md5($_SERVER['HTTP_USER_AGENT']);
+        $salt = $user_agent ?? uniqid();
+        $data['token'] = sha1($salt.time().bin2hex(random_bytes(16)));
+        $data['user_id'] = $user_id;
 
-        return $this->encode($user);
+        return $data;
     }
 
-    public function generateRefreshToken()
+    public function createRefreshToken($user_id)
     {
-        $salt = $_SERVER['HTTP_USER_AGENT'] ?? uniqid();
-        return sha1($salt.time().bin2hex(random_bytes(16)));
+        $data = $this->generateRefreshToken($user_id);
+        $this->model->create($data);
+
+        return $data['token'];
     }
 
-    private function encode(stdClass $user, ?int $iat = null)
+    public function checkRefreshToken($token)
+    {
+        $user_agent = md5($_SERVER['HTTP_USER_AGENT']);
+        $lifetime = $this->config['refresh_lifetime'];
+        $row = $this->model->read($token, $user_agent, $lifetime);
+
+        $this->deleteOrUpdate($token, $row);
+
+        return $row;
+    }
+  
+    public function encodeJWT(User $user, ?int $iat = null)
     {
         $payload = [
             'iss' => $this->config['iss'],
@@ -43,16 +60,32 @@ class O2AuthRepo
                 'name' => $user->name,
             ]
         ];
-
+        
         return JWT::encode($payload, $this->config['key'], $this->config['algo']);
     }
-
-    private function decode(string $jwt)
+    
+    public function decodeJWT(string $jwt): object|bool
     {
         try {
             return JWT::decode($jwt, new Key($this->config['key'], $this->config['algo']));
-        } catch (Throwable $e) {
-            return null;
+        } catch (ExpiredException $e) {
+            return true;
+        } catch (UnexpectedValueException $e) {
+            return false;
+        }
+    }
+
+    private function deleteOrUpdate($token, $row)
+    {
+        if ($row) {
+            $refresh_period = $this->config['refresh_update'];
+            $last_activity = strtotime($row->updated);
+            if ($last_activity + $refresh_period < time()) {
+                $data = $this->generateRefreshToken($row->user_id);
+                $this->model->update($token, ['token' => $data['token']]);
+            }
+        } else {
+            $this->model->delete($token);
         }
     }
 }
