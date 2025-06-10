b52595557c4702e79f30411d844d71fa20a5dcbc
alx <world.zay@gmail.com>
2025-06-10 12:25:38 +0300

    Create validation i18n files

---
+++ application/config/messages/en.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'isUniqueEmail'   => 'User with this email is already registered',
+    'isRegisteredEmail' => 'This email address not found',
+    'isPairEmailPswd' => 'The email/password pair does not match',
+    'isUniqueOrOwnEmail' => 'User with this email is already registered',
+];
