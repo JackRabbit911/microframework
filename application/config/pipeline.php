213f05689add6c2ace4278947855dee175fe4510
alx <world.zay@gmail.com>
2025-06-11 11:17:03 +0300

    Update pipelene config

--- application/config/pipeline.php
+++ application/config/pipeline.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 // use App\Middleware\MockRequest;
 use Sys\Middleware\CORS;
 
-$this->pipeline->pipe(MockRequest::class);
+// $this->pipeline->pipe(MockRequest::class);
 $this->pipeline->pipe(CORS::class);
