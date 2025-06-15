854d9c9ed20d5492f01b0850248ca46ea2b4cbc7
alx <world.zay@gmail.com>
2025-06-15 14:41:08 +0300

    Update routes config

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,12 +3,14 @@
 use App\Controller\Auth;
 use App\Controller\File;
 use App\Controller\Home;
+use App\Controller\Page;
 use HttpSoft\Response\JsonResponse;
 use Sys\Console\Controller as ConsoleController;
 
 return [
     'console'   => ['/console/{model}/{method}', ConsoleController::class, ['model' => '[\w\/]+']],
     'auth'      => ['/auth/{action}', Auth::class],
+    'page'      => ['/page/{action}', Page::class],
     'file'      => ['/file/{file}', File::class, ['file' => '.*']],
     'home'      => ['/{action?}/{id?}', Home::class],
 ];
